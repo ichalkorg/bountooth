d58c702fd8aa709ec3c00c9deb12d8899a190770
ichalkorg <55353741+ichalkorg@users.noreply.github.com>
2021-03-05 14:33:51 +0700

    Update safe_utama.js

--- safe_utama.js
+++ safe_utama.js
@@ -10,7 +10,8 @@ var Base64 = {_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123
     enc1 = chr1 >> 2; 
     enc2 = ((chr1 & 3) << 4) | (chr2 >> 4); 
     enc3 = ((chr2 & 15) << 2) | (chr3 >> 6); 
-    enc4 = chr3 & 63; if (isNaN(chr2)) {
+    enc4 = chr3 & 63; 
+    if (isNaN(chr2)) {
       enc3 = enc4 = 64;
     } else if (isNaN(chr3)) {
       enc4 = 64;
