a5e94a228a42bd55407961cbe02a0baab59b0dc6
ichalkorg <55353741+ichalkorg@users.noreply.github.com>
2021-03-06 00:17:06 +0700

    Update coba.js

--- coba.js
+++ coba.js
@@ -110,7 +110,7 @@ function auto_safeconvert() {
     } if (a_to_vh == false) {
       var encryptedUrl = Base64.encode(a_to_vi); 
       a_to_ve[i].href = xxx + "?url=" + encryptedUrl; 
-      a_to_ve[i].rel = "noopener noreferrer";
+      a_to_ve[i].rel = "nofollow";
       a_to_vb++; 
       a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
     }
